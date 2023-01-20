e5cd79a81bc99922833e233b085f5e5868f8e474
Kartik Chaturvedi <kchaturvedi@users.noreply.github.com>
2023-01-19 19:10:34 -0500

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest } from 'next'
 
-type Data = {
-  name: string
+export const config = { runtime: 'edge' }
+
+const handler = async (req: NextApiRequest) => {
+  const { headers } = req
+  const response = await fetch(`https://api.weatherapi.com/v1/ip.json?q=30024&key=${process.env.OPEN_WEATHER_KEY}`)
+  const conditions = await response.json()
+  console.log(conditions)
+  const data = JSON.stringify({ conditions })
+  return new Response(data, { headers: { 'Content-Type': 'application/json; charset=utf-8' } })
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler
